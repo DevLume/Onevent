2333ee5d9bac88a083c056461071fd47e9e8d571
Deividas Šimkus <deividas.simkus@mif.stud.vu.lt>
2018-09-24 18:32:48 +0300

    initial release

---
+++ Onevent/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Onevent.Startup))]
+namespace Onevent
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
