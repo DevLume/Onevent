f31a8454e67e36798b7a5bad171915bdeeb69567
Deividas Šimkus <deividas.simkus@mif.stud.vu.lt>
2018-10-07 21:49:29 +0300

    created data model for events database with entity framework

---
+++ Onevent/App_Code/Models/EventContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+namespace Onevent.Models
+{
+    public class EventContext : DbContext
+    {
+        public EventContext() : base("Onevent")
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Event> Events { get; set; }
+    }
+}
